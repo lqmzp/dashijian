b7e29a4e7c5bf85af6dcd696746e5d235f0f41bd
zsc <2792643139@qq.com>
2022-08-04 11:19:16 +0800

    公共部分完成

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+const getUserInfo = () => {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        data: '',
+        success: (res) => {
+            if (res.status !== 0) return layui.layer.msg("数据请求失败！");
+            // console.log(res);
+            // 调用 renderAvatar 渲染用户头像
+            renderAvatar(res.data);
+        },
+    });
+}
+
+const renderAvatar = (data) => {
+    let name = data.nickname || data.username;
+    // 设置欢迎文本
+    // console.log(name);
+    $("#welcome").html(`欢迎 ${name}`);
+    // 按需渲染用户头像
+    if (data.user_pic !== null) {
+        // 渲染图片头像
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        // 渲染文本头像
+        $(".layui-nav-img").hide();
+        let firstName = name[0].toUpperCase();
+        $(".text-avatar").html(firstName);
+    }
+}
+$('#exitBtn').on('click', function () {
+    console.log(1);
+    layui.layer.confirm('确定退出？', { icon: 3, title: '' }, function () {
+        location.href = '/login.html'
+        localStorage.removeItem("token");
+    })
+})
+getUserInfo()
